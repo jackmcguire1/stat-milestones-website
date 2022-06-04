0f0f4a7e5e0ae8134c785d6d1aabbd7fcee42a2b
jack <jackmcguire35@gmail.com>
2022-06-04 17:25:03 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar color="purple" elevation="4">
+        <v-appbar-title> <b> Stat-Milestones </b></v-appbar-title>
+
+        <v-row align="right" justify="right">
+          <v-col></v-col>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+            align="center"
+            justify="center"
+            ><v-divider vertical></v-divider>
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-row>
+      </v-app-bar>
+
+      <v-container align="center" justify="center" v-if="!loadedChannels">
+        <v-progress-circular
+          :size="70"
+          :width="7"
+          color="purple"
+          indeterminate
+        ></v-progress-circular>
+
+        <v-container></v-container>
+      </v-container>
+
+      <v-row>
+        <div v-for="channel in channels">
+          <v-col sm="1">
+            <Profile :channel="channel" :show="show"></Profile
+          ></v-col>
+        </div>
+      </v-row>
+
+      <v-divider></v-divider>
+      <Timeline></Timeline>
+      <v-footer dark justify="center">
+        <v-container fluid>
+          <v-row align="center" justify="center">
+            <v-col></v-col>
+            <v-divider></v-divider>
+            {{ new Date().getFullYear() }} — <strong>Stat-Milestones</strong>
+          </v-row>
+        </v-container>
+      </v-footer>
+
+      <v-snackbar v-model="snackbar.status" :timeout="snackbar.timeout">
+        Successfully loaded {{ channels.length }} Twitch streams!
+
+        <template v-slot:action="{ attrs }">
+          <v-btn
+            color="red"
+            text
+            v-bind="attrs"
+            @click="snackbar.status = false"
+          >
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Profile from "@/components/Profile.vue";
+import axios from "axios";
+export default {
+  name: "App",
+
+  components: { Profile },
+  mounted() {
+    this.getData();
+  },
+  data: () => ({
+    snackbar: {
+      status: false,
+      timeout: 10000,
+    },
+    channels: [],
+    show: false,
+    loadedChannels: false,
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+  methods: {
+    getData: function () {
+      let response = axios
+        .get(
+          "https://6gywu6gbge.execute-api.us-east-1.amazonaws.com/Prod/live-channels?channel-id=?e93cf8730nd11z7gepkly2gry5kv8k"
+        )
+        .then((response) => {
+          this.channels = response.data.channels;
+          this.loadedChannels = true;
+          this.snackbar.status = true;
+        });
+    },
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-container>
+    <v-sheet
+      color="white"
+      elevation="14"
+      height="700"
+      outlined
+      rounded
+      width="600"
+    >
+      <v-container>
+        <v-card class="mx-auto">
+          <v-toolbar color="purple" elevation="4" flat outlined rounded>
+            <v-toolbar-title>
+              <b> {{ channel.broadcaster_name }} </b>
+            </v-toolbar-title>
+
+            <v-spacer></v-spacer>
+            <v-avatar>
+              <img :src="channel.profile" :alt="channel.broadcaster_id" />
+            </v-avatar>
+          </v-toolbar>
+
+          <v-card-title> {{ channel.game_name }} </v-card-title>
+
+          <v-card-text>
+            <v-divider></v-divider>
+            <v-container>
+              <iframe
+                v-bind:src="
+                  `https://player.twitch.tv/?channel=` +
+                  channel.broadcaster_name +
+                  `&parent=jackmcguire1.github.io&autoplay=false`
+                "
+                allowfullscreen
+                height="300px"
+                width="500px"
+              >
+              </iframe>
+            </v-container>
+            <v-container align="center"> {{ channel.title }}</v-container>
+            <v-divider></v-divider>
+            <v-container> Viewers: {{ channel.viewers }}</v-container>
+            <v-divider></v-divider>
+            <v-container>
+              Twitch Partnership Status:
+              <b>{{ channel.type.toUpperCase() }}</b></v-container
+            >
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-sheet>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Profile",
+  data: () => ({
+  }),
+  props: {
+    channel: {},
+    show: Boolean,
+  },
+};
+</script>
+<style scoped>
+</style>
