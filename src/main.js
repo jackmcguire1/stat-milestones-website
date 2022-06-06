243bd3ad34f60701951a19e27eab226e3c0da7f3
Jack McGuire <jackmcguire35@gmail.com>
2022-06-06 03:48:07 +0100

    feat: google analytics
    - Add vue-gtag to start tracking google analytics metrics

--- src/main.js
+++ src/main.js
@@ -5,9 +5,13 @@ import { loadFonts } from './plugins/webfontloader'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
 import.meta.hot
+import VueGtag from "vue-gtag";
 
 loadFonts()
 
 createApp(App)
   .use(vuetify)
+  .use(VueGtag, {
+    config: { id: "G-WZZT87HP70" }
+  })
   .mount('#app')
