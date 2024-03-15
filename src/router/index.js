67137ab72b4f8d8bbf26695f97f8445cab8cc74c
Jack McGuire <jackmcguire35@gmail.com>
2024-03-15 12:22:59 +0000

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Channels from '../views/Channels.vue'
 import Playground from '../views/Playground.vue'
 import Analytics from '../views/Analytics.vue'
@@ -28,7 +28,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
